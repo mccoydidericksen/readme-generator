60f4486f58b41983a6fe079d746d89d76fe61bb3
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-23 17:31:45 -0700

    add questions

--- index.js
+++ index.js
@@ -2,13 +2,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+ {
+    message: 'What is the title of your project?',
+    name: 'title'
+  },
+  {
+    message: 'Enter a description for your project:',
+    name: 'desc'
+  },
+  {
+    message: 'Enter installation instructions:',
+    name: 'install'
+  },
+  {
+    message: 'Enter usage information:',
+    name: 'usage'
+  },
+  {
+    message: 'Enter contribution guidelines:',
+    name: 'contribution'
+  },
+  {
+    message: 'Enter test instructions:',
+    name: 'test'
+  },
+  {
+    message: 'Enter your github username:',
+    name: 'username'
+  },
+  {
+    message: 'Enter your email address:',
+    name: 'email'
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((resp) => {
+            
+        });
+}
 
 // Function call to initialize app
 init();
